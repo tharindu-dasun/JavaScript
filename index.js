f2a91fa69d603a20f5c4e7010a910004a4e526e7
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-07 01:30:00 +0530

    Arithmetic expression

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ console.log(drive)
 document.getElementById("p1").innerHTML = "Hello " + firstName;
 document.getElementById("p2").innerHTML = "Age " + age;
 document.getElementById("p3").innerHTML = "Drive " + drive;
+
+// Arithmatic expression
+
+let students = 20;
+
+students = students + 1;
+students = students - 1;
+students = students * 1;
+students = students / 1;
+students = students % 1;
+
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students %= 3;
+
+let result = 5 + 5 * (3+4);
+console.log(result);
