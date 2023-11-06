4e85aa3edaeec8dfad3586b5ef467da0724a8899
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-07 01:22:14 +0530

    variables

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log("Hello");
 window.alert("Success !")
+
+let firstName = "tharindu";
+let age = 22 ;
+let drive = true;
+
+console.log(firstName)
+console.log(age)
+console.log(drive)
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "Age " + age;
+document.getElementById("p3").innerHTML = "Drive " + drive;
