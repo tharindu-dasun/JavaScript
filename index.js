12c928573e2e88d3e8101beaab43214a74bcbcb6
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-07 19:07:34 +0530

    Type Conversion

--- index.js
+++ index.js
@@ -2,45 +2,62 @@ console.log("Hello");
 window.alert("Success !")
 
 let firstName = "tharindu";
-let age = 22 ;
-let drive = true;
-
-console.log(firstName)
-console.log(age)
-console.log(drive)
-
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "Age " + age;
-document.getElementById("p3").innerHTML = "Drive " + drive;
+// let age = 22 ;
+// let drive = true;
+//
+// console.log(firstName)
+// console.log(age)
+// console.log(drive)
+//
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "Age " + age;
+// document.getElementById("p3").innerHTML = "Drive " + drive;
 
 // Arithmatic expression
 
-let students = 20;
-
-students = students + 1;
-students = students - 1;
-students = students * 1;
-students = students / 1;
-students = students % 1;
-
-students += 1;
-students -= 1;
-students *= 2;
-students /= 2;
-students %= 3;
-
-let result = 5 + 5 * (3+4);
-console.log(result);
+// let students = 20;
+//
+// students = students + 1;
+// students = students - 1;
+// students = students * 1;
+// students = students / 1;
+// students = students % 1;
+//
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students %= 3;
+//
+// let result = 5 + 5 * (3+4);
+// console.log(result);
 
 //User Input
 
 // let userName = window.prompt("Your Name ?");
 // console.log(userName);
 
-let userName;
+// let userName;
+//
+// document.getElementById("btn").onclick = function (){
+//     userName = document.getElementById("my").value;
+//     console.log(userName);
+//     document.getElementById("myLabel").innerHTML = "Hello "+userName;
+// }
 
-document.getElementById("btn").onclick = function (){
-    userName = document.getElementById("my").value;
-    console.log(userName);
-    document.getElementById("myLabel").innerHTML = "Hello "+userName;
-}
+//Type Conversion
+
+// let age = window.prompt("How old are u?")
+// console.log(typeof age);
+// age = Number(age);
+// console.log(typeof age);
+//
+// console.log("Happy " + age + " Birthday !")
+
+let x = Number("2.55");
+let y = String("2.55");
+let z = Boolean("AAA");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
